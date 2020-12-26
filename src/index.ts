93eb577b8b338ed4d4b9fad8755f07a599f0c1f6
rtang03 <>
2020-12-26 21:04:22 +0800

    debug ci

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { EventStore } from './contract/eventstore';
-import { PrivateData } from './contract/privateData';
+import { PrivateData } from './contract/privatedata';
 import { BaseEvent, Commit, createInstance } from './ledger-api';
 
 export { BaseEvent, EventStore, Commit, createInstance };
